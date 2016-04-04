ab8ad6eb403910239509651588ed59adb8508bee
Chris Jones <chris@axismedtech.com>
2016-04-04 14:17:32 -0600

    wip

---
+++ src/main/java/showcase/client/ShowcaseEntryPoint.java
@@ -0,0 +1,10 @@
+package showcase.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class ShowcaseEntryPoint implements EntryPoint {
+    @Override
+    public void onModuleLoad() {
+        // todo
+    }
+}
---
+++ src/main/java/showcase/resources/ShowcaseResourceBundle.java
@@ -0,0 +1,8 @@
+package showcase.resources;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+
+public interface ShowcaseResourceBundle extends ClientBundle {
+    ShowcaseResourceBundle INSTANCE = GWT.create(ShowcaseResourceBundle.class);
+}
